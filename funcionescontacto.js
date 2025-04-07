ff1776ee3352d329fb1505be258552698e3c299f
sofia <est.nikol.forero@unimilitar.edu.co>
2025-04-07 00:44:35 -0500

    se arreglo el aspecto de la venta

--- funcionescontacto.js
+++ funcionescontacto.js
@@ -1,3 +1,19 @@
+// Mostrar/ocultar menú del usuario
+document.addEventListener("DOMContentLoaded", function () {
+  const userBtn = document.querySelector('.user-btn');
+  const userMenu = document.querySelector('.user-menu');
+
+  userBtn.addEventListener('click', function () {
+      userMenu.classList.toggle('show');
+  });
+
+  // Cerrar el menú si se hace clic fuera
+  window.addEventListener('click', function (e) {
+      if (!userBtn.contains(e.target) && !userMenu.contains(e.target)) {
+          userMenu.classList.remove('show');
+      }
+    });
+});
 document.addEventListener("DOMContentLoaded", function () {
   const formulario = document.getElementById("formularioContacto");
 
