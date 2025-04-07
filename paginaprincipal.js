96cfc4106d132e4d2004687bd0bcfe6b88712d32
Andres Edilson Briceño Monroy <est.andrese.briceno@unimilitar.edu.co>
2025-04-07 02:59:59 -0500

    finalizado

--- paginaprincipal.js
+++ paginaprincipal.js
@@ -30,6 +30,32 @@ function cambiarImagen(direccion) {
   document.getElementById("imagen-hero").src = imagenes[indice];
 }
 
+//  Funcionalidad del botón "Suscríbete"
+const botonSuscribirse = document.getElementById('boton-suscribirse');
+if (botonSuscribirse) {
+    botonSuscribirse.addEventListener('click', () => {
+        window.location.href = 'suscribirse.html'; // Reemplaza con tu ruta real
+    });
+}
+});
+
+
+
+// Mostrar/ocultar menú del usuario
+document.addEventListener("DOMContentLoaded", function () {
+const userBtn = document.querySelector('.user-btn');
+const userMenu = document.querySelector('.user-menu');
+
+userBtn.addEventListener('click', function () {
+    userMenu.classList.toggle('show');
+});
+
+// Cerrar el menú si se hace clic fuera
+window.addEventListener('click', function (e) {
+    if (!userBtn.contains(e.target) && !userMenu.contains(e.target)) {
+        userMenu.classList.remove('show');
+    }
+    });
 
 });
 
